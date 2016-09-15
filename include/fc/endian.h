700c918164cc03af9113eee1dc32a5e73f3b4ed5
Dag-Erling Smørgrav <d.e.smorgrav@usit.uio.no>
2016-09-15 11:49:22 +0200

    Implement the full range of BSD endianness conversion functions.

--- include/fc/endian.h
+++ include/fc/endian.h
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2014 Dag-Erling Smørgrav
+ * Copyright (c) 2016 Universitetet i Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -38,6 +39,15 @@
 #include <endian.h>
 #endif
 
+#if !HAVE_DECL_BSWAP16
+#define bswap16 fc_bswap16
+#endif
+#if !HAVE_DECL_BSWAP32
+#define bswap32 fc_bswap32
+#endif
+#if !HAVE_DECL_BSWAP64
+#define bswap64 fc_bswap64
+#endif
 #if !HAVE_DECL_BE16ENC
 #define be16enc fc_be16enc
 #endif
@@ -74,6 +84,83 @@
 #if !HAVE_DECL_LE64DEC
 #define le64dec fc_le64dec
 #endif
+#if !HAVE_DECL_HTOBE16
+#define htobe16 fc_htobe16
+#endif
+#if !HAVE_DECL_BE16TOH
+#define be16toh fc_be16toh
+#endif
+#if !HAVE_DECL_HTOLE16
+#define htole16 fc_htole16
+#endif
+#if !HAVE_DECL_LE16TOH
+#define le16toh fc_le16toh
+#endif
+#if !HAVE_DECL_HTOBE32
+#define htobe32 fc_htobe32
+#endif
+#if !HAVE_DECL_BE32TOH
+#define be32toh fc_be32toh
+#endif
+#if !HAVE_DECL_HTOLE32
+#define htole32 fc_htole32
+#endif
+#if !HAVE_DECL_LE32TOH
+#define le32toh fc_le32toh
+#endif
+#if !HAVE_DECL_HTOBE64
+#define htobe64 fc_htobe64
+#endif
+#if !HAVE_DECL_BE64TOH
+#define be64toh fc_be64toh
+#endif
+#if !HAVE_DECL_HTOLE64
+#define htole64 fc_htole64
+#endif
+#if !HAVE_DECL_LE64TOH
+#define le64toh fc_le64toh
+#endif
+
+static inline uint16_t
+fc_bswap16(uint16_t u16)
+{
+#if HAVE___BUILTIN_BSWAP16
+	return (__builtin_bswap16(u16));
+#else
+	return (((u16 & 0x00ffU) >> 0) << 8 |
+	    ((u16 & 0xff00U) >> 8) << 0);
+#endif
+}
+
+static inline uint32_t
+fc_bswap32(uint32_t u32)
+{
+#if HAVE___BUILTIN_BSWAP32
+	return (__builtin_bswap32(u32));
+#else
+	return (((u32 & 0x000000ffLU) >> 0) << 24 |
+	    ((u32 & 0x0000ff00LU) >> 8) << 16 |
+	    ((u32 & 0x00ff0000LU) >> 16) << 8 |
+	    ((u32 & 0xff000000LU) >> 24) << 0);
+#endif
+}
+
+static inline uint64_t
+fc_bswap64(uint64_t u64)
+{
+#if HAVE___BUILTIN_BSWAP64
+	return (__builtin_bswap64(u64));
+#else
+	return (((u64 & 0x00000000000000ffLLU) >> 0) << 56 |
+	    ((u64 & 0x000000000000ff00LLU) >> 8) << 48 |
+	    ((u64 & 0x0000000000ff0000LLU) >> 16) << 40 |
+	    ((u64 & 0x00000000ff000000LLU) >> 24) << 32 |
+	    ((u64 & 0x000000ff00000000LLU) >> 32) << 24 |
+	    ((u64 & 0x0000ff0000000000LLU) >> 40) << 16 |
+	    ((u64 & 0x00ff000000000000LLU) >> 48) << 8 |
+	    ((u64 & 0xff00000000000000LLU) >> 56) << 0);
+#endif
+}
 
 static inline void
 fc_be16enc(void *p, uint16_t u16)
@@ -189,6 +276,114 @@ fc_le64dec(const void *p)
 	    (uint64_t)((const uint8_t *)p)[5] << 40 |
 	    (uint64_t)((const uint8_t *)p)[6] << 48 |
 	    (uint64_t)((const uint8_t *)p)[7] << 56);
+}
+
+static inline uint16_t
+fc_htobe16(uint16_t u16)
+{
+#if !WORDS_BIGENDIAN
+	u16 = fc_bswap16(u16);
+#endif
+	return (u16);
+}
+
+static inline uint16_t
+fc_be16toh(uint16_t u16)
+{
+#if !WORDS_BIGENDIAN
+	u16 = fc_bswap16(u16);
+#endif
+	return (u16);
+}
+
+static inline uint16_t
+fc_htole16(uint16_t u16)
+{
+#if WORDS_BIGENDIAN
+	u16 = fc_bswap16(u16);
+#endif
+	return (u16);
+}
+
+static inline uint16_t
+fc_le16toh(uint16_t u16)
+{
+#if WORDS_BIGENDIAN
+	u16 = fc_bswap16(u16);
+#endif
+	return (u16);
+}
+
+static inline uint32_t
+fc_htobe32(uint32_t u32)
+{
+#if !WORDS_BIGENDIAN
+	u32 = fc_bswap32(u32);
+#endif
+	return (u32);
+}
+
+static inline uint32_t
+fc_be32toh(uint32_t u32)
+{
+#if !WORDS_BIGENDIAN
+	u32 = fc_bswap32(u32);
+#endif
+	return (u32);
+}
+
+static inline uint32_t
+fc_htole32(uint32_t u32)
+{
+#if WORDS_BIGENDIAN
+	u32 = fc_bswap32(u32);
+#endif
+	return (u32);
+}
+
+static inline uint32_t
+fc_le32toh(uint32_t u32)
+{
+#if WORDS_BIGENDIAN
+	u32 = fc_bswap32(u32);
+#endif
+	return (u32);
+}
+
+static inline uint64_t
+fc_htobe64(uint64_t u64)
+{
+#if !WORDS_BIGENDIAN
+	u64 = fc_bswap64(u64);
+#endif
+	return (u64);
+}
+
+static inline uint64_t
+fc_be64toh(uint64_t u64)
+{
+#if !WORDS_BIGENDIAN
+	u64 = fc_bswap64(u64);
+#endif
+	return (u64);
+}
+
+static inline uint64_t
+fc_htole64(uint64_t u64)
+{
+#if WORDS_BIGENDIAN
+	u64 = fc_bswap64(u64);
+#endif
+	return (u64);
+}
+
+static inline uint64_t
+fc_le64toh(uint64_t u64)
+{
+#if WORDS_BIGENDIAN
+	u64 = fc_bswap64(u64);
+#endif
+	return (u64);
 }
 
 #endif
